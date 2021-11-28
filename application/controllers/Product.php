0f6c861fa22072691d177fba2ecaea29821ce104
Adhika Guna <adhikag167@gmail.com>
2021-11-28 20:51:35 +0700

    update

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -1,40 +1,103 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Product extends CI_Controller {
+class Product extends CI_Controller
+{
 
     public $firebaseConn;
 
     public function __construct()
     {
-            parent::__construct();
-            $this->load->library('firebase');
-            $this->load->model('m_base');
+        parent::__construct();
+        $this->load->library('firebase');
+        $this->load->library('form_validation');
+        $this->load->model('m_base');
 
-		    $firebaseConn = $this->firebase->init();
+        $this->firebaseConn = $this->firebase->init();
     }
 
-	public function index()
-	{
-  
-	}
+    public function index()
+    {
+    }
 
     public function requestproduct()
-	{
-        
+    {
+
         $this->load->view('template/header_view.php');
         $this->load->view('request_product.php');
         $this->load->view('template/footer_view.php');
-	}
+    }
+
+    public function insertProduct()
+    {
+        $post = $this->input->post();
+
+        $this->form_validation->set_rules('product_name', 'Product Name', 'required|trim');
+        $this->form_validation->set_rules('starting_price', 'Starting Price', 'required|trim');
+        $this->form_validation->set_rules('end_date', 'End Date', 'required|trim');
+
+        if (!empty($_FILES['product_image']['name'])) {
+            $config['upload_path']          = './assets/image';
+            $config['allowed_types']        = 'jpg|png|jpeg';
+            $config['max_size']             = 2000;
+            $config['max_width']            = 0;
+            $config['max_height']           = 0;
+            $config['overwrite']            = FALSE;
+            $config['remove_spaces']        = TRUE;
+            
+            $this->load->library('upload', $config);
+    
+
+            if ($this->form_validation->run() == false) {
+                $this->requestproduct();
+            } else {
+                if (!$this->upload->do_upload('product_image')) {
+                    print_r($this->upload->display_errors());
+                    $this->requestproduct();
+                } else {
+                    //upload to firebase
+                    $defaultBucket = $this->firebaseConn->getStorage();
+                    $name = $_FILES['product_image']['name'];
+
+
+                    $uploadedfile = fopen('./assets/image/'.$name, 'r');
+
+                    $uploadedName = $this->upload->data('raw_name').time().$this->upload->data('file_ext');
+
+                    $defaultBucket->getBucket()->upload($uploadedfile, ['name' =>  $uploadedName]);
+
+                    unlink('./assets/image/'.$name);
+
+                    $image_url = 'https://firebasestorage.googleapis.com/v0/b/auction-website-1cc67.appspot.com/o/' . $uploadedName;
+
+                    $data = array(
+                        'name' => $post['product_name'],
+                        'starting_price' => $post['starting_price'],
+                        'end_date' => $post['end_date'],
+                        'user_id' => $this->session->userdata('id'),
+                        'product_image'  => $image_url,
+                    );
+
+                    $this->m_base->insertTable('product_bid', $data);
+
+                    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+					Congratulation! your product has been submitted, the process of checking taking 24hour.
+					</div>');
+
+                    redirect(base_url('product/requestproduct'));
+                }
+            }
+        }
+    }
 
     public function myproduct()
-	{
+    {
         $id = $this->session->userdata('id');
 
         $product['data'] = $this->m_base->getDetail('product_bid', $id)->result_array();
-      
+
         $this->load->view('template/header_view.php');
-        $this->load->view('my_product.php',$product);
+        $this->load->view('my_product.php', $product);
         $this->load->view('template/footer_view.php');
-	}
+    }
 }
--- application/views/request_product.php
+++ application/views/request_product.php
@@ -1,32 +1,32 @@
 <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-8">
-
+            <?= $this->session->flashdata('message'); ?>
                 <div class="card rounded shadow">
                     <div class="card-header">
                         Request Product
                     </div>
                     <div class="card-body">
-                        <form @submit.prevent="onUpload()">
+                          <?= form_open_multipart(base_url('product/insertproduct')) ?>
                             <div class="mb-3">
                                 <label for="" class="form-label">Product Name</label>
-                                <input type="text" class="form-control" v-model="transaction.product_name">
-                               
+                                <input type="text" class="form-control"  value="<?php echo set_value('product_name'); ?>" name="product_name" required>
+                                <?= form_error('name', '<small class="text-danger pl-3">', '</small>') ?>
                             </div>
                                <div class="mb-3">
                                 <label for="" class="form-label">Starting Price</label>
-                                <input type="number" class="form-control" v-model="transaction.starting_price">
-                        
+                                <input type="number" class="form-control"  value="<?php echo set_value('starting_price'); ?>" name="starting_price" required>
+                                <?= form_error('starting_price', '<small class="text-danger pl-3">', '</small>') ?>
                             </div>
                                <div class="mb-3">
                                 <label for="" class="form-label">End Date</label>
-                                
-                                <input type="date" class="form-control" placeholder="yyyy-mm-dd hh:mm:ss" v-model="transaction.end_date">
+                                <input type="date" class="form-control"  value="<?php echo set_value('end_date'); ?>" placeholder="yyyy-mm-dd hh:mm:ss" name="end_date" required>
+                                <?= form_error('end_date', '<small class="text-danger pl-3">', '</small>') ?>
                             
                             </div>
                               <div class="mb-3">
                                 <label for="" class="form-label">Product Images</label>
-                                <input type="file" name="imagesArray[]" class="form-control" multiple @change="onChange">
+                                <input type="file" name="product_image" class="form-control">
                             </div>
                             <button class="btn btn-outline-primary">Submit</button>
                         </form>
