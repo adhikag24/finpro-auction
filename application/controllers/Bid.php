cc647371979b16c62c6ed8ed269b421ae2afbf9b
Adhika Guna <adhikag167@gmail.com>
2021-12-09 14:05:47 +0700

    update

--- application/controllers/Bid.php
+++ application/controllers/Bid.php
@@ -1,37 +1,58 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Bid extends CI_Controller {
+class Bid extends CI_Controller
+{
 
     public $firebaseConn;
 
     public function __construct()
     {
-            parent::__construct();
-            $this->load->library('firebase');
-            $this->load->model('m_base');
-		    $firebaseConn = $this->firebase->init();
+        parent::__construct();
+        $this->load->library('firebase');
+        $this->load->model('m_base');
+        $firebaseConn = $this->firebase->init();
     }
 
-	public function index()
-	{
-        
- 
-	}
+    public function index()
+    {
+    }
 
     public function mybid()
-	{
+    {
         $id = $this->session->userdata('id');
-        $condition = array('id' =>$id);
-        
+        $condition = array('id' => $id);
+
         $bid['data'] = $this->m_base->getOneToMany('bid', 'product_bid', 'product_id', 'id', $condition)->result_array();
 
         $this->load->view('template/header_view.php');
-        $this->load->view('my_bid.php',$bid);
+        $this->load->view('my_bid.php', $bid);
         $this->load->view('template/footer_view.php');
-	}
+    }
 
-    public function syncbidwinner(){
-        echo "hi";
+    public function syncbidwinner()
+    {
+        $products = $this->m_base->getWhere('product_bid', ['is_active' => 1])->result_array();
+
+        foreach ($products as $product) {
+            $today = date("Y-m-d");
+            if ($product['end_date'] < $today) {
+                $bidData = $this->m_base->getWhere('bid', ['product_id' => $product['id']])->result_array();
+                foreach ($bidData as $bid) {
+                    if ($bid['amount'] == $product['highest_bid']) {
+                        echo "Winner " . $bid['user_id'];
+                        $check = $this->m_base->countWhere('bid_winner', ['product_id' => $product['id'], 'user_id' => $bid['user_id']]);
+                        if ($check == 0) {
+                            $data = [
+                                'user_id'   => $bid['user_id'],
+                                'product_id'   => $product['id']
+                            ];
+                            $insert = $this->m_base->insertTable('bid_winner', $data);
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
